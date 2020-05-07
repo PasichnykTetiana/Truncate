c83fda2a690c37a34706e6117c50fa2568205c8d
Tetiana <t.pasichnyk@ukr.net>
2020-05-07 23:36:48 +0300

    complete

--- Truncate.js
+++ Truncate.js
@@ -4,16 +4,21 @@ function truncateString(str, num) {
     for(i=0; i<num; i+=1){
         newstr+=str[i];
     }
-    console.log(newstr);
+    // console.log(newstr.length> str.length);
     if (newstr.length == str.length){
+        console.log('равно')
         return str
-        console.log(str)
+    }
+    else if (newstr.length > str.length){
+        console.log('больше на 2')
+        return str
     }
     else {
+        console.log('меньше')
         return newstr + '...'
-        console.log(newstr + '...')
+
     }
 
 }
-
+// console.log("A-tisket a-tasket A green and yellow basket".length);
 truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2);
